9e7c96999f7928c8dd58418935908e7461e7e694
Mari Garman <garman.mariana@gmail.com>
2019-05-26 04:18:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,9 +8,9 @@ class School
 
   def add_student(name, grade)
     if roster[grade]
-      roster[grade] << (name)
+      roster[grade] << (name) 
       else 
-      roster[grade] = [name]
+      roster[grade] = name
     end 
   end
 
